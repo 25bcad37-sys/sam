758a2f8722bf28a290ed2b5f625a45230b49b778
Melwin Sam Thomas <25bcad37@kristujayanti.com>
2026-03-26 21:50:27 +0530

    Initial portfolio with Flask and MySQL

--- app.py
+++ app.py
@@ -34,4 +34,7 @@ def contact():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
